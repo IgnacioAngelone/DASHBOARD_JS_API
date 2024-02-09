041fdf28d180e255b75892365fa7131b0cb4890c
IgnacioAngelone <nachoangelone@gmail.com>
2024-02-09 04:14:30 -0300

    #2 Folder structure

---
+++ Source-code/src/index.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const Index = () => {
+     return (
+         <div>
+            
+         </div>
+     );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Index />);
