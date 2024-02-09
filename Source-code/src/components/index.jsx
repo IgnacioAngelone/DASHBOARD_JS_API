d10eb9cf232f17b833e7ef8b292f0fddfdbc23a3
IgnacioAngelone <nachoangelone@gmail.com>
2024-02-09 15:35:09 -0300

    #1 Components - template shop

--- Source-code/src/components/index.jsx
+++ Source-code/src/components/index.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import TemplateShop from './template-shop';
 
 export const Index = () => {
      return (
-         <div>
-             <p>Hello, world!</p>
-         </div>
+         <TemplateShop />
      );
 };
 
---
+++ Source-code/src/components/template-shop.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "../styles/template-shop.css";
+
+export default function Shop() {
+    return (
+         <div>
+            
+         </div>
+    );
+}
