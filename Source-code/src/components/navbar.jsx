59af797fe17f053dbd7ac730edd2b51f3123d6af
IgnacioAngelone <nachoangelone@gmail.com>
2024-02-15 16:45:38 -0300

    #11 Components - all components

--- Source-code/src/components/navbar.jsx
+++ Source-code/src/components/navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
                  <li><a href="https://ar.linkedin.com/in/ignacioangelone" target="_blank">CONTACT</a></li>
              </ol>
 
-             <a href="https://www.epicgames.com/site/es-ES/home" target="_blank" className="nav_img"><img src="assets/images/Logo-epicpng.png" alt="logo de epic games" /></a>
+             <a href="https://www.epicgames.com/site/es-ES/home" target="_blank" className="nav_img"><img src="assets/images/Logo-epicgames.webp" alt="logo de epic games" /></a>
 
              <ol>
                  <li><a href="https://ignacioangelone.github.io/Mariano-Fortnite/" target="_blank">HOME</a></li>
