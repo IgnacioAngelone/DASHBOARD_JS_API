073dea3f9f07ea7819ef6d32955a4e1c89448386
IgnacioAngelone <nachoangelone@gmail.com>
2024-02-15 15:12:59 -0300

    #7 Components - link fix

--- Source-code/src/components/navbar.jsx
+++ Source-code/src/components/navbar.jsx
@@ -6,17 +6,17 @@ export default function Navbar() {
     return(
         <nav className="nav">
             <ol>
-                 <li><a href="#home">OFICIAL</a></li>
-                 <li><a href="#about">GAMES</a></li>
-                 <li><a href="#contact">CONTACT</a></li>
+                 <li><a href="https://www.fortnite.com/">OFICIAL</a></li>
+                 <li><a href="https://store.epicgames.com/en-US/">GAMES</a></li>
+                 <li><a href="https://ar.linkedin.com/in/ignacioangelone">CONTACT</a></li>
              </ol>
 
-             <a href="" className="nav_img"><img src="assets/images/Logo-epic.webp" alt="" /></a>
+             <a href="https://www.epicgames.com/site/es-ES/home" target="_blank" className="nav_img"><img src="assets/images/Logo-epic.webp" alt="logo de epic games" /></a>
 
              <ol>
-                 <li><a href="#home">HOME</a></li>
-                 <li><a href="#about">SHOP</a></li>
-                 <li><a href="#contact">MAP</a></li>
+                 <li><a href="https://ignacioangelone.github.io/Mariano-Fortnite/">HOME</a></li>
+                 <li><a href="https://ignacioangelone.github.io/Mariano-Fortnite/">SHOP</a></li>
+                 <li><a href="https://ignacioangelone.github.io/Mariano-Fortnite/">MAP</a></li>
             </ol>
         </nav>
     )
